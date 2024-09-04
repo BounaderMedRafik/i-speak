49e284310e222092c1f9d14cfbb9a51dc8e89fb9
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-09-04 17:26:10 +0100

    new things

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Feutres from "@/components/marketing/Feutres";
+import Gallery from "@/components/marketing/Gallery";
 import { Hero } from "@/components/marketing/Hero";
 
 export default function Home() {
@@ -10,6 +11,10 @@ export default function Home() {
       <div>
         <Feutres />
       </div>
+      <div>
+        <Gallery />
+      </div>
+      <div className=" h-screen" />
     </main>
   );
 }
